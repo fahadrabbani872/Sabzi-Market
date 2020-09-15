ce612a8c998789e57a0f073d2c420120039a35aa
Fahad Zahid <Fahadzahid0310@gmail.com>
2020-09-15 23:38:03 +0500

    frist commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var kiaChahiye = prompt("Han bhai kia chahiye")
+    
+    if (kiaChahiye == "aloo") {
+        var aloo = prompt("Aloo 50 Rupee kilo hai Kitni chahiye")
+    } 
+        if (aloo == "1 kilo") {
+            alert("1 kilo aloo ho gaya")
+    }   else {
+            alert("Nahi ha")
+    }
+
+var orKiaChahiye = prompt("Or kia chahiye")
+     if (orKiaChahiye == "piyaz") {
+        var piyaz = prompt("piyaz 30 Rupee kilo hai Kitni chahiye")
+    }
+        if (piyaz == "1 kilo") {
+            alert("1 kilo ho gaya")
+    }   else {
+            alert("Nahi ha")
+    }   
+
+var orKuchChahiye = prompt("Or kuch chahiye")
+    if (orKuchChahiye == "nahi") {
+        var nahi = alert("Total ap k paise (80) rupee ho gae")
+        alert("Thankyou for shopping")
+    }
